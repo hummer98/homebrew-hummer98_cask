3e0d03218ec3059cbad95e1f9c68fcd39a04292d
yuji.yamamoto <yuji.yamamoto@k.s-cubism.jp>
2014-02-06 15:00:56 +0900

    クラス名修正

--- Casks/openoffice-lang.rb
+++ Casks/openoffice-lang.rb
@@ -1,4 +1,4 @@
-class Openoffice < Cask
+class OpenofficeLang < Cask
   url 'http://sourceforge.net/projects/openofficeorg.mirror/files/4.0.1/binaries/ja/Apache_OpenOffice_4.0.1_MacOS_x86_install_ja.dmg'
   homepage 'http://www.openoffice.org/'
   version '4.0.1'
