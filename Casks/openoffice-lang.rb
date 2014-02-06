1787c4bb4c428c2994648a9043e6850ca7f43373
yuji.yamamoto <yuji.yamamoto@k.s-cubism.jp>
2014-02-06 14:55:52 +0900

    URL修正

--- Casks/openoffice-lang.rb
+++ Casks/openoffice-lang.rb
@@ -1,5 +1,5 @@
 class Openoffice < Cask
-  url 'http://sourceforge.net/projects/openofficeorg.mirror/files/4.0.1/binaries/ja/Apache_OpenOffice_4.0.1_MacOS_x86_install_ja.dmg/download'
+  url 'http://sourceforge.net/projects/openofficeorg.mirror/files/4.0.1/binaries/ja/Apache_OpenOffice_4.0.1_MacOS_x86_install_ja.dmg'
   homepage 'http://www.openoffice.org/'
   version '4.0.1'
   sha1 'f44460d11785b2086970f8262e3e811dfd8bc9a4'
