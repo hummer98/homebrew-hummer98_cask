10185131ec2ba0dce582d7313bdcab95e126d185
yuji.yamamoto <yuji.yamamoto@k.s-cubism.jp>
2014-02-06 14:34:06 +0900

    日本語版をダウンロードするように修正

---
+++ Casks/openoffice-lang.rb
@@ -0,0 +1,7 @@
+class Openoffice < Cask
+  url 'http://sourceforge.net/projects/openofficeorg.mirror/files/4.0.1/binaries/ja/Apache_OpenOffice_4.0.1_MacOS_x86_install_ja.dmg/download'
+  homepage 'http://www.openoffice.org/'
+  version '4.0.1'
+  sha1 'f44460d11785b2086970f8262e3e811dfd8bc9a4'
+  link 'OpenOffice.app'
+end
--- README.md
+++ README.md
@@ -2,3 +2,8 @@ homebrew-hummer98_cask
 ======================
 
 My alternative Casks
+
+
+# install
+
+    brew tap hummer98/homebrew-hummer98_cask
