f1753b18d075a91d1b097edd593905f70f0fa2b2
yuji.yamamoto <yuji.yamamoto@k.s-cubism.jp>
2014-03-03 19:53:31 +0900

    gifzo.rbにsha1追加

--- Casks/gifzo.rb
+++ Casks/gifzo.rb
@@ -2,5 +2,6 @@ class Gifzo < Cask
   url 'http://gifzo.net/Gifzo.zip'
   homepage 'http://gifzo.net/'
   version '1.0.2'
+  sha256 'c4a521ad9fdbebda8efad5aadfd2cf43b8ac31c778795eba67ebbf8928a12f19'
   link 'Gifzo.app'
 end
