15599b1dff80d6cd21080609254e38ac60f3d0d2
yuji.yamamoto <yamamoto@ridgeroot.com>
2014-03-06 20:30:15 +0900

    fixed classname

--- Casks/firefox-jp.rb
+++ Casks/firefox-jp.rb
@@ -1,4 +1,4 @@
-class Firefox < Cask
+class FirefoxJp < Cask
   url 'http://download-installer.cdn.mozilla.net/pub/firefox/releases/27.0.1/mac/ja-JP-mac/Firefox%2027.0.1.dmg'
   homepage 'http://www.mozilla.org/ja/firefox/'
   version '27.0.1'
