e3b98d23b7b36065ebb690f9afbc7ac37b559cfb
yuji.yamamoto <yuji.yamamoto@k.s-cubism.jp>
2014-03-06 17:43:48 +0900

    add senebier.rb

---
+++ Casks/senebier.rb
@@ -0,0 +1,7 @@
+class Senebier < Cask
+  url 'http://www.bicoid.com/app/senebier/archive/senebier.dmg'
+  homepage 'http://www.bicoid.com/app/senebier/index.html'
+  version '0.95.0'
+  no_checksum
+  link 'Senebier.app'
+end
