dbe87f9c668548e06b9518da7fa069290a204ade
yuji.yamamoto <yamamoto@ridgeroot.com>
2014-03-07 09:02:52 +0900

    firefox-jp.rb : fixed sha1

--- Casks/firefox-jp.rb
+++ Casks/firefox-jp.rb
@@ -2,5 +2,6 @@ class FirefoxJp < Cask
   url 'http://download-installer.cdn.mozilla.net/pub/firefox/releases/27.0.1/mac/ja-JP-mac/Firefox%2027.0.1.dmg'
   homepage 'http://www.mozilla.org/ja/firefox/'
   version '27.0.1'
+  sha256 'ebd451f28a5ddab819e85ffc26d3bbdb852f5106704deda62b6b15cce745bea2'
   link 'Firefox.app'
 end
