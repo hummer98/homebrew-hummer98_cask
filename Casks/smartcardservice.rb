b8966801ef4e8efd110892ec68b7bd5e9a481422
yuji.yamamoto <yamamoto@ridgeroot.com>
2014-03-11 16:24:28 +0900

    add smartcardservice.rb

---
+++ Casks/smartcardservice.rb
@@ -0,0 +1,6 @@
+class Smartcardservice < Cask
+  url 'http://smartcardservices.macosforge.org/files/installers/SmartCardServices2.0.1-OSX10.9.zip'
+  homepage 'http://smartcardservices.macosforge.org/trac/wiki/installers'
+  version '2.0.1'
+  sha1 '3a2af6d30628d141decf228f61608f8bf260b7b3'
+end
