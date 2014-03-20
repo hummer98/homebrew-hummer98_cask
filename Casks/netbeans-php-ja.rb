7a85a6cd1eb0411625534049bb712f40528e3600
yuji.yamamoto <yamamoto@ridgeroot.com>
2014-03-20 12:18:20 +0900

    fixed sha256

--- Casks/netbeans-php-ja.rb
+++ Casks/netbeans-php-ja.rb
@@ -2,7 +2,7 @@ class NetbeansPhpJa < Cask
   url 'http://download.netbeans.org/netbeans/8.0/final/bundles/netbeans-8.0-php-macosx.dmg'
   homepage 'https://netbeans.org/downloads/?pagelang=ja'
   version '8.0'
-  md5 'afd57068aed462948624ee6ce41a34dc'
+  sha256 'b33830cb86bc5d626717a9dbbd0a94bf37530b8cfd06b1e4526b060921115ec5'
   install 'NetBeans 8.0.mpkg'
   uninstall(
     :pkgutil => 'org.netbeans.ide.*|glassfish-.*',
